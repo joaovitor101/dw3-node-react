23ee7e1830d498898f6f5af8e067843b2266f0a8
joaovitor101 <joao.kusaka27@gmail.com>
2025-03-11 18:57:42 -0300

    11.03.2025

---
+++ api-the-games/services/gameService.js
@@ -0,0 +1,21 @@
+import Game from "../models/Games.js"
+
+class gameService {
+    // Game.find().then(games => {
+    // //sucesso
+    // }).catch(error => {
+    // // falha
+    // })
+
+    // async / await
+    async getAll() {
+        try {
+            const games = await Game.find()
+            return games
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+export default new gameService()
