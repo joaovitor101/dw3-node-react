87fb29d2472544bbcf02e045f011acc0a3c7f1ef
joaovitor101 <joao.kusaka27@gmail.com>
2025-03-19 19:44:34 -0300

    Conexão banco Mongo Atlas

---
+++ api-the-games-mongo-db-atlas/config/db-connection.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const dbUser = "joaokusaka27";
+const dbPassword = "RqBmBXmmAifk7SWx";
+
+const connect = () => {
+  mongoose.connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.o1nu8.mongodb.net/api-the-games?retryWrites=true&w=majority&appName=Cluster0`
+  );
+  const connection = mongoose.connection;
+  connection.on("error", () => {
+    console.log("Erro ao conectar ao MongoDB");
+  });
+  connection.on("open", () => {
+    console.log("Conectado ao MongoDB");
+  });
+};
+connect();
+export default mongoose;
